8a030d17ed85d0a31396c9977490c2524b299ea0
Josh Barr <Josh.Barr@uline.com>
2018-11-20 17:20:14 -0600

    starting to create the $resources methods for calling the REST service

---
+++ APM/APM.Client/APM.Client/common/common.services.js
@@ -0,0 +1,12 @@
+﻿(function () {
+	"use strict";
+
+	angular
+		.module("common.services",
+					["ngResource"])
+		.constant("appSettings",
+		{
+			//localhost path for the Web API portion
+			serverPath: "http://localhost:53874/"
+		});
+}());
